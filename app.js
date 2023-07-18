9bf4d391f12db565de07ff47a184d599aaa726d8
Tomas Ossa Cardenas <tomasossaefcsl@gmail.com>
2023-07-18 11:44:34 -0400

    All views were updated using bootstrap to have a more user friendly interface.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 const path=require('path');
 const mongoose=require('mongoose');
 const methodOverride=require('method-override');
+const ejsMate=require('ejs-mate');
 const Campground = require('./models/campground');
 
 //Connection to the database
@@ -20,8 +21,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp',{
 
 app.set('views', path.join(__dirname, 'views')); //Set the views folder where the html will be
 app.set('view engine', 'ejs'); //Needed to indicate that ejs will be the template
+app.engine('ejs', ejsMate);
 
-app.use(express.urlencoded({extended:true})); //This is needed to parse the req.body
+//MIDDLEWARE
+app.use(express.urlencoded({extended:true})); //This is needed to parse the req.body (What it does is every single request is being url encoded by express function)
 app.use(methodOverride('_method'));//To be able to fake PUT or PATCH requests
 
 //ROUTES
@@ -40,8 +43,8 @@ app.get('/campgrounds/new', async (req,res)=>{
 });
 //post for sending data
 app.post('/campgrounds', async (req,res)=>{ //Called when the form is sent
-    const {title, location}=req.body.campground; //In the new.ejs form we have title and location encapsulated in the campground[attribute]
-    const newCampground=new Campground({title:title, location:location});
+    const {title, location, image, price, description}=req.body.campground; //In the new.ejs form we have title and location encapsulated in the campground[attribute]
+    const newCampground=new Campground({title:title, location:location, image:image, price:price, description:description});
     await newCampground.save();
     res.redirect(`campgrounds/${newCampground._id}`);
 });
@@ -53,33 +56,22 @@ app.get('/campgrounds/:id/edit', async (req,res)=>{ //Called when button edit is
 
 //Single campground info
 app.get('/campgrounds/:id', async (req,res)=>{
-    const camp= await Campground.findById(req.params.id);
-    res.render('./campground/show', {camp})
+    const campground= await Campground.findById(req.params.id);
+    res.render('./campground/show', {campground})
 });
 //Update a campground
 app.put('/campgrounds/:id', async (req,res)=>{
-    const {title, location}=req.body.campground;
-    await Campground.findByIdAndUpdate(req.params.id,{title:title, location:location});
+    const {title, location, image, price, description}=req.body.campground;
+    await Campground.findByIdAndUpdate(req.params.id,{title:title, location:location, image:image,price:price, description:description});
     res.redirect(`./${req.params.id}`);
 });
-//delete
+//delete a campground
 app.delete('/campgrounds/:id', async (req,res)=>{ //Called when form delete is executed by the button delete
     const campground=await Campground.findByIdAndDelete(req.params.id);
     res.redirect('/campgrounds');
 });
 
 
-
-app.get('/makeCampground', async (req,res)=>{
-    const camp=new Campground({
-        title: 'MyBACKYARD',
-        price:"50",
-        description:"my campground",
-        location:"Dont know"
-    });
-    await camp.save()
-    res.send(camp)
-});
 
 //Server
 app.listen(3000, ()=>{
